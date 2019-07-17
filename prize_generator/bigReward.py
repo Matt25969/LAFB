a7d9ac0840ee41579dc3c36890c2f832319400e2
Monika-Mistry <monika.mistry1@gmail.com>
2019-07-17 12:00:07 +0100

    multiple implementation

---
+++ prize_generator/bigReward.py
@@ -0,0 +1,17 @@
+import os
+import random
+from flask import Flask
+app - Flask (__name__)
+
+@app.route(/prize)
+def prize():
+	x = random.randint(1,101)
+	if x <= 75:
+		return "£0"
+	else:
+		return "£50"
+	
+	
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 9000))
+    app.run(host='0.0.0.0', port=port)
