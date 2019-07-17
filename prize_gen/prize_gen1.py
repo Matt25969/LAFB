c37449102c564b99e16adae6b5cfd7bd43cfc3eb
Ubuntu <krystak@project.kqrawb24gvru3l4yue0n3n4q2a.ax.internal.cloudapp.net>
2019-07-17 15:06:55 +0000

    get request workingggg

--- prize_gen/prize_gen1.py
+++ prize_gen/prize_gen1.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, make_response
 import random
+import requests
 from random import randrange
 prize_gen = Flask(__name__)
 
@@ -9,7 +10,7 @@ def reset(prob=25):
     prize=50
     percent = random.randrange(100)
     if prob > percent:
-	
+        request.get('http://52.142.204.121:9000/notify')
         return jsonify({"User has won":prize})
     else:
         return "No prize for you"
