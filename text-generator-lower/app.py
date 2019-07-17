e7312a02131df9256b25962f37fd7a4e9f224ce9
phileasfoggy22 <phileas.foggy@googlemail.com>
2019-07-17 14:50:41 +0100

    python numtextgenerators

---
+++ text-generator-lower/app.py
@@ -0,0 +1,35 @@
+#!flask/bin/python
+from flask import Flask, jsonify, make_response
+import sys
+import requests
+import random
+import string
+from random import randint
+app = Flask(__name__)
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    return "test"
+
+
+@app.route('/numgen/', methods=['GET'])
+def text_gen():
+    numbers = ''
+    for x in range(6):
+        numbers += str(random.randint(0, 9))
+    return jsonify({"Random numbers": numbers})
+
+
+@app.route('/anEndpoint')
+def make_request():
+    return requests.get('http://example.com').content
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0', port=9017)
