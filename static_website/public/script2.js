d3857441fa6f0b14e7dda1276bebf47fbc109196
Chris-McQuiggan <50955666+Chris-McQuiggan@users.noreply.github.com>
2019-07-18 17:35:46 +0100

    Create script2.js

---
+++ static_website/public/script2.js
@@ -0,0 +1,48 @@
+
+function getAPrize() {
+
+    let accountDetails = {
+        "accountnumber": "";
+        "firstName": document.getElementById("FirstNameBox").value;
+        "lastName": document.getElementById("LastNameBox").value;
+        "prize": "";
+    }
+
+    let req = new XMLHttpRequest();
+
+    req.onload = function () {
+        promises(req);
+    }
+    
+    req.open("POST", "server/addAccount");
+    req.send(JSON.stringify(accountDetails));
+}
+
+function promises(req) {
+    const createPromise = new Promise(
+        function (res, rej) {
+            if (req.status === 200) {
+                let result = JSON.parse(req.responseText);
+                res(result);
+            } else {
+                const reason = new Error(req.status);
+                rej(reason);
+            }
+        }
+    );
+
+    createPromise
+        .then(result => resolved(result))
+        .catch(error => rejected(error))
+
+}
+
+function resolved(result) {
+    let response = JSON.parse(req.response);
+    console.log(response);
+}
+
+function rejected(reason) {
+    console.log(reason);
+}
+        
