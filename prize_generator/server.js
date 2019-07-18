67a707fa5ebcbd35bb43fa670c42f0b59f597596
Monika-Mistry <monika.mistry1@gmail.com>
2019-07-18 13:29:52 +0000

    added server.js

---
+++ prize_generator/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//Two implementation of prize generation: smallReward (less generous), bigReward (more generous)
+//const reward = require("./routes/smallReward");
+const reward = require("./routes/bigReward");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/prize", reward);
+
+const port = process.env.PORT || 5002;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
