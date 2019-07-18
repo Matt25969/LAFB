63d55c8d78bb2fecf82c2cf4d1f5226fff4114b0
Chris-McQuiggan <50955666+Chris-McQuiggan@users.noreply.github.com>
2019-07-18 11:58:54 +0100

    Update app.py

--- prize-generator-small/app.py
+++ prize-generator-small/app.py
@@ -51,4 +51,5 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=9017)
+    port = int(os.environ.get("PORT", 50003))
+    app.run(host='0.0.0.0', port=port)
