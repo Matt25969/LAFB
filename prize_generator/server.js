5501339f71dd78f12c0390931a49d9fcfdcd86f3
Monika-Mistry <Monika-Mistry@users.noreply.github.com>
2019-07-18 14:39:13 +0100

    Update server.js

--- prize_generator/server.js
+++ prize_generator/server.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 
 //Two implementation of prize generation: smallReward (less generous), bigReward (more generous)
 //const reward = require("./routes/smallReward");
-const reward = require("./routes/bigReward");
+//const reward = require("./routes/bigReward");
+const reward = require("./routes/reward");
 
 const app = express();
 
