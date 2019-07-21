6b4d729774931bdd9c377eb87b51fe827468e04e
ayshamarty <ayshamarty@gmail.com>
2019-07-21 15:09:08 +0100

    installation scripts

---
+++ installation/jenkins.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+sudo apt update
+sudo apt install -y openjdk-8-jdk
+sudo apt install -y maven
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt update
+sudo apt install -y jenkins
+
+sudo usermod -aG docker Jenkins
+
+sudo chmod 777 /var/lib/jenkins/secrets/initialAdminPassword
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+echo "Please restart your terminal"
